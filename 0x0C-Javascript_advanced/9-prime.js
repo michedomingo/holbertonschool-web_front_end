f2ff81a17ad36ded9f450aeb9418e67331b85cc3
michedomingo <michedomingo@gmail.com>
2021-02-18 17:02:05 -0800

    Task9 solution

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,23 @@
+function countPrimeNumbers() {
+  let count = 0;
+  const prime = new Array(100).fill(true);
+
+  for (let i = 2; i < 100; i++) {
+    if (prime[i] === true) {
+      count++;
+      for (let j = 2; i * j < 100; j++) {
+        prime[i * j] = false;
+      }
+    }
+  }
+
+  return count;
+}
+
+let t0 = performance.now();
+countPrimeNumbers();
+let t1 = performance.now();
+
+console.log(
+  `Execution time of printing countPrimeNumbers was ${t1 - t0} milliseconds.`
+);
