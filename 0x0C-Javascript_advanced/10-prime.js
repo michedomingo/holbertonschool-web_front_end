af406d0cecf3339152259a32d20de9903b1e0ee2
michedomingo <michedomingo@gmail.com>
2021-02-18 17:02:16 -0800

    Task10 solution

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,27 @@
+function countPrimeNumbers() {
+  let count = 0;
+  const prime = new Array(100).fill(true);
+
+  for (let i = 2; i < 100; i++) {
+    if (prime[i] === true) {
+      count++;
+      for (let j = 2; i * j < 100; j++) {
+        prime[i * j] = false;
+      }
+    }
+  }
+
+  return count;
+}
+
+let t0 = performance.now();
+for (let i = 0; i < 100; i++) {
+  countPrimeNumbers();
+}
+let t1 = performance.now();
+
+console.log(
+  `Execution time of calculating prime numbers 100 times was ${
+    t1 - t0
+  } milliseconds.`
+);
